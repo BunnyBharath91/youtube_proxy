67f187952b0deb6e57d88a8fa26f6ec34d06b7ca
bharath kumar borra <bunnybharath917@gmail.com>
2024-07-26 15:39:04 +0530

    after adding domain as .onrender.com

--- backend/index.js
+++ backend/index.js
@@ -85,7 +85,7 @@ app.use(
       secure: true, // Set to true in production for HTTPS
       sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000, // Session valid for 30 days
-      domain: 'youtube-proxy-frontend.onrender.com', // Corrected placement
+      domain: '.onrender.com', // Corrected placement
       path:'/'
     },
     
