0ac3ca19ba8a9197eb3ec33d78e2169cbf5efc63
harshini <harshini.ravula@nxtwave.co.in>
2024-07-29 10:50:58 +0530

    change domain name to frontend app domain

--- backend/index.js
+++ backend/index.js
@@ -86,7 +86,7 @@ app.use(
       secure: true, // Set to true in production for HTTPS
       sameSite: false,
       maxAge: 30 * 24 * 60 * 60 * 1000, // Session valid for 30 days
-      domain: 'onrender.com', // Corrected placement
+      domain: 'youtube-proxy-frontend.onrender.com', // Corrected placement
       path:'/'
     },
     
