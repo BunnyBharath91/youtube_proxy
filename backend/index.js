244359937863d0830c37b9ff61b76e9d7be537d7
harshini <harshini.ravula@nxtwave.co.in>
2024-07-29 12:28:18 +0530

    update domain

--- backend/index.js
+++ backend/index.js
@@ -81,10 +81,9 @@ app.use(
     cookie: {
       httpOnly: true,
       secure: true, // Set to true in production for HTTPS
-      sameSite: "None",
+      sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000, // Session valid for 30 days
-      domain: "onrender.com",
-      path: "/", // Corrected placement
+      domain: ".onrender.com",
     },
   })
 );
